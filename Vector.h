703dd5f83e54f54bd0205c9b46960de9fb8ba381
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-10-01 00:05:20 -0500

    Create Vector.h

---
+++ Vector.h
@@ -0,0 +1,22 @@
+//vector.h
+class Point{
+public:
+	double x,y;
+};
+class Vector{
+public:
+	Point start, end;
+	int distancia(Vector vec1.end.x, vec1.start.x, vec1.end.y, vec1.start.y){
+		int result;
+		result=sqrt(pow(((vec1.end.x)-(vec1.start.x)),2)+pow(((vec1.end.y)-(vec1.start.y)),2));
+		return result;
+	};
+class Circulo{
+public:
+	float radio;
+	float area(Circulo rad){
+		int areacirculo;
+		areacirculo=Pi*pow(rad.2);
+		return areacirculo;
+	}
+};
