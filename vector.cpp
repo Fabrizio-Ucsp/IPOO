05594a37bdd9541917bf91d667aed107b782767e
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-10-01 20:35:28 -0500

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,15 +1,14 @@
-#include "vector.h"
-#include "vector.cpp"
 #include <iostream>
 #include <math.h>
+using namespace std;
 double Vector::distance(){
 	return sqrt(pow((start.x - end.x),2) + pow((start.y - end.y),2) );
 }
 void Vector::print(){
-	cout<< start.x <<endl;
-	cout<< start.y <<endl;
-	cout<< end.x <<endl;
-	cout<< end.y <<endl;
+	cout<< "el valor de x inicial es: "<<start.x <<endl;
+	cout<< "el valor de y inicial es: "<<start.y <<endl;
+	cout<< "el valor de x final es: "<<end.x <<endl;
+	cout<< "el valor de y final es: "<<end.y <<endl;
 }
 double Circulo::area(){
 	return 3.14*pow(radio,2);
