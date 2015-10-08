f4aee80ed3b56d52b969291d85060778f9060b9d
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-10-07 19:20:04 -0500

    Update Lista de Ejercicios Ejercicio 10.cpp

--- Lista de Ejercicios Ejercicio 10.cpp
+++ Lista de Ejercicios Ejercicio 10.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
-#include<ctime>
-#include<stdlib.h>
-using namespace std ;
-int main () { 
-	int num;
-	int list[400000];
-	cout<<"Ingrese un numero: "<<endl;
-	cin>>num;
-	for(int i=0;i<400000;i++){
-		list[i]=rand()%100;
-		cout<<list[i]<<endl;
-	}
-	for (int i=0;i<400000;i++){
-		if (num == i){
-			cout<<"el numero se encuentra en el arreglo"<<endl;
-		}
-	}
+#include <fstream>
+#include <string>
+#include <iostream>	
+using namespace std;
+string impr(string line);
+int main () {
+	string line;
+	impr(line);
+	return 0;
 }
+string impr(string line){
+	 ifstream myfile ("texto.txt");
+	 if (myfile.is_open())
+	 {
+		 while ( getline (myfile,line) )
+		 {
+			 cout << line << '\n';
+		 }
+		 myfile.close();
+	 }
+ }
