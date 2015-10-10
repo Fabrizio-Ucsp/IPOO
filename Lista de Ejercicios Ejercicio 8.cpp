e934a12c85796b468bdd86fbaec58fd125a8eebf
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-10-09 22:19:45 -0500

    Update Lista de Ejercicios Ejercicio 8.cpp

--- Lista de Ejercicios Ejercicio 8.cpp
+++ Lista de Ejercicios Ejercicio 8.cpp
@@ -1,23 +1,25 @@
 #include <ctype.h> 
 #include <iostream>
+#define tam 200
 using namespace std;
 int mayus();
 int main() { 
-	char nombre[100]; 
+	char nombre[tam]; 
 	mayus();
 	return 0; 
 }
 int mayus(){
-	char nombre[100]; 
+	char nombre[tam]; 
 	char *pnombre;
 	pnombre=nombre;
 	cout<<"Ingrese una frase: "<<endl;; 
-	cin.getline(nombre,100);
+	cin.getline(nombre,tam);
 	nombre[0]=toupper(nombre[0]);
 	for (int i=0;i<100;i++){
 		if (nombre[i] == ' '){
 			nombre[i+1]=toupper(nombre[i+1]);
 		}
 	}
+	cout<<"La frase modificada seria: "<<endl;
 	cout<<nombre<<endl;
 }
