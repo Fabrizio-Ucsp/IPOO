bdd46d3edd5e1631fc4ad9bd693aea1fe0d1f453
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-10-09 22:17:23 -0500

    Update Lista de Ejercicios Ejercicio 8.cpp

--- Lista de Ejercicios Ejercicio 8.cpp
+++ Lista de Ejercicios Ejercicio 8.cpp
@@ -1,11 +1,23 @@
 #include <ctype.h> 
 #include <iostream>
 using namespace std;
+int mayus();
 int main() { 
 	char nombre[100]; 
-	cout<<"Ingrese una palabra: "<<endl;; 
-	cin>>nombre;
-	nombre[0]=toupper(nombre[0]);
-	cout<<"La palabra nueva seria: "<<nombre<<endl; 
+	mayus();
 	return 0; 
+}
+int mayus(){
+	char nombre[100]; 
+	char *pnombre;
+	pnombre=nombre;
+	cout<<"Ingrese una frase: "<<endl;; 
+	cin.getline(nombre,100);
+	nombre[0]=toupper(nombre[0]);
+	for (int i=0;i<100;i++){
+		if (nombre[i] == ' '){
+			nombre[i+1]=toupper(nombre[i+1]);
+		}
+	}
+	cout<<nombre<<endl;
 }
