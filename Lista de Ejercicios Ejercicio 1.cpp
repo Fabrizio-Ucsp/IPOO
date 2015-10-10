30c6fd3daff95e24d93cc6b124eb31954e32296e
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2015-10-09 20:08:46 -0500

    Update Lista de Ejercicios Ejercicio 1.cpp

--- Lista de Ejercicios Ejercicio 1.cpp
+++ Lista de Ejercicios Ejercicio 1.cpp
@@ -1,63 +1,86 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
-	int A[40][40], B[40][40], C[40][40];
-	int a, b, c;
-	cout<<"Filas de Matriz A: "; cin>>a;
-	cout<<"Columnas de Matriz A: "; cin>>b;
-	cout<<endl;
-	for(int i=0; i<a; ++i)
-		for(int j=0; j<b; ++j)
-		{
-		cout<<"Ingrese valor para Matriz A "<<i<<","<<j<<": ";
-		cin>>A[i][j];
+int **MatrizA, **MatrizB, **MatrizC;
+int FilasDeLaMatrizA, ColumnasDeLaMatrizA, ColumnasDeLaMatrizB;
+void CreaciondelaMatrizA(){
+	MatrizA = new int *[FilasDeLaMatrizA];
+	for(int i = 0; i<FilasDeLaMatrizA; i++){
+		MatrizA[i] = new int[ColumnasDeLaMatrizA];
 	}
-	cout<<"Matriz  B."<<endl;
-	cout<<"Filas de Matriz B: "<<b<<endl;
-	cout<<"Columnas de Matriz B: "; cin>>c;
-	cout<<endl;
-	for(int i=0; i<b; ++i)
-		for(int j=0; j<c; ++j)
-		{
-		cout<<"Ingrese valor para Matriz B"<<i<<","<<j<<": ";
-		cin>>B[i][j];
+	for(int i = 0; i<FilasDeLaMatrizA; i++){
+		for(int j = 0; j<ColumnasDeLaMatrizA; j++){
+			cout<<"Valor "<<"("<<i+1<<","<<j+1<<")"<<": ";
+			cin>>MatrizA[i][j];
+		}
 	}
-	for(int i=0; i<a; ++i)
-		for(int j=0; j<c; ++j)
-			C[i][j] = 0;
-	for(int i=0; i<a; ++i)
-		for(int j=0; j<c; ++j)
-			for(int z=0; z<b; ++z)
-				C[i][j] += A[i][z] * B[z][j];
-	
-	cout<<"Matriz A: ";
-	for(int i=0; i<a; ++i)
-	{
-		for(int j=0; j<b; ++j)
-		{
-			cout<<A[i][j]<<" ";
+	cout<<" "<<endl;
+	cout<<"Matriz A:"<<endl;
+	cout<<" "<<endl;
+	for(int i = 0; i<FilasDeLaMatrizA; i++){
+		for(int j = 0; j<ColumnasDeLaMatrizA; j++){
+			cout<<MatrizA[i][j]<<" ";
+		}
+		cout<<" "<<endl;;
+	}
+}
+void CreaciondelaMatrizB(){
+	MatrizB = new int *[ColumnasDeLaMatrizA];
+	for(int i = 0; i<ColumnasDeLaMatrizA; i++)    {
+		MatrizB[i] = new int[ColumnasDeLaMatrizB];
+	}
+	for(int i = 0; i<ColumnasDeLaMatrizA; i++){
+		for(int j = 0; j<ColumnasDeLaMatrizB; j++){
+			cout<<"Valor "<<"("<<i+1<<","<<j+1<<")"<<": ";
+			cin>>MatrizB[i][j];
+		}
+	}
+	cout<<" "<<endl;
+	cout<<"Matriz B:"<<endl;
+	cout<<" "<<endl;
+	for(int i = 0; i<ColumnasDeLaMatrizA; i++){
+		for(int j = 0; j<ColumnasDeLaMatrizB; j++){
+			cout<<MatrizB[i][j]<<" ";
+		}
+		cout<<" "<<endl;
+	}
+}
+void productMatrix(){
+	MatrizC = new int *[FilasDeLaMatrizA];
+	for(int i = 0; i<FilasDeLaMatrizA; i++){
+		MatrizC[i] = new int[ColumnasDeLaMatrizB];
+	}
+	for(int i=0; i<FilasDeLaMatrizA; ++i){
+		for(int j=0; j<ColumnasDeLaMatrizB; ++j){
+			MatrizC[i][j] = 0;
+		}
+	}
+	for(int i=0; i<FilasDeLaMatrizA; ++i){
+		for(int j=0; j<ColumnasDeLaMatrizB; ++j){
+			for(int z=0; z<ColumnasDeLaMatrizA; ++z){
+				MatrizC[i][j] += MatrizA[i][z] * MatrizB[z][j];
+			}
+		}
+	}
+	cout<<" "<<endl;
+	cout<<"El Producto de las 2 Matrices es:"<<endl;
+	for(int i=0; i<FilasDeLaMatrizA; ++i){
+		for(int j=0; j<ColumnasDeLaMatrizB; ++j){
+			cout<<MatrizC[i][j]<<" ";
 		}
 		cout<<endl;
 	}
-	cout<<"Matriz B: ";
-	for(int i=0; i<b; ++i)
-	{
-		for(int j=0; j<c; ++j)
-		{
-			cout<<B[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-	cout<<"Matriz C: ";
-	for(int i=0; i<a; ++i)
-	{
-		for(int j=0; j<c; ++j)
-		{
-			cout<<C[i][j]<<" ";
-		}
-		cout<<endl;
-	}
+}
+int main(){
+	cout<<"Numero de filas de la Matriz A: ";
+	cin>>FilasDeLaMatrizA;
+	cout<<"Numero de columnas de la Matriz A: ";
+	cin>>ColumnasDeLaMatrizA;
+	CreaciondelaMatrizA();
+	cout<<" "<<endl;;
+	cout<<"Numero de filas de la Matriz B: "<<ColumnasDeLaMatrizA<<endl;
+	cout<<"Numero de Columnas de la Matriz B: ";
+	cin>>ColumnasDeLaMatrizB;
+	CreaciondelaMatrizB();
+	productMatrix();
 	return 0;
 }
